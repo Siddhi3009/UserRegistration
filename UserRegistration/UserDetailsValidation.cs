24c47d276aea3b36a53aff231b1fc0b230afa3e6
Administrator <siddhiseth3009@gmail.com>
2020-10-07 01:22:09 +0530

    [Siddhi]Refactor Regex statements edited

--- UserRegistration/UserDetailsValidation.cs
+++ UserRegistration/UserDetailsValidation.cs
@@ -7,7 +7,7 @@ namespace UserRegistration
 {
     class UserDetailsValidation
     {
-        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
         public bool validateFirstName(string fName)
         {
             return Regex.IsMatch(fName, REGEX_FIRST_NAME);
