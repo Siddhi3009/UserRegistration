5757581e829cc090e432358d1662e4bd3cff8ea4
Administrator <siddhiseth3009@gmail.com>
2020-10-07 01:22:46 +0530

    [Siddhi]Refactor Regex statements edited

--- UserRegistration/UserDetailsValidation.cs
+++ UserRegistration/UserDetailsValidation.cs
@@ -7,8 +7,8 @@ namespace UserRegistration
 {
     class UserDetailsValidation
     {
-        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
-        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
         public bool validateFirstName(string fName)
         {
             return Regex.IsMatch(fName, REGEX_FIRST_NAME);
