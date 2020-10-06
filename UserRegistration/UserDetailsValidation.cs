503a90020a206bbb786687c8236c3d33ad63f363
Administrator <siddhiseth3009@gmail.com>
2020-10-07 00:57:49 +0530

    [Siddhi]Refactor. Password validation added for exactly 1 special character

--- UserRegistration/UserDetailsValidation.cs
+++ UserRegistration/UserDetailsValidation.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
         public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
         public static string REGEX_EMAIL = "^[a-z]{2,10}[_+-.]?[a-z0-9]{0,10}?@[a-z]{2,10}.[a-z]{2,10}[.]?[a-z]{2}?";
         public static string REGEX_NUMBER = "^[1-9]{1}[0-9]{1}\\s[1-9]{1}[0-9]{9}$";
-        public static string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9]).{8,20}";
+        public static string REGEX_PASSWORD = "(?=.*[A-Z])(?=.*[0-9])(?=.?[@#$%^&-+=().]{1}).{8,}";
         public bool validateFirstName(string fName)
         {
             return Regex.IsMatch(fName, REGEX_FIRST_NAME);
