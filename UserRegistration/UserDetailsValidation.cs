532d830e1db9e95942cf9b458da882bb9a0e0959
Administrator <siddhiseth3009@gmail.com>
2020-10-07 01:32:52 +0530

    [Siddhi]Refactor Regex statements edited

--- UserRegistration/UserDetailsValidation.cs
+++ UserRegistration/UserDetailsValidation.cs
@@ -7,9 +7,9 @@ namespace UserRegistration
 {
     class UserDetailsValidation
     {
-        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
-        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,10}$";
-        public static string REGEX_EMAIL = "^[a-z]{2,10}[_+-.]?[a-z0-9]{0,10}?@[a-z]{2,10}.[a-z]{2,10}[.]?[a-z]{2}?";
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_EMAIL = "^[a-z]{2,}[_+-.]?[a-z0-9]{0,}?@[a-z]{2,}.[a-z]{2,}[.]?[a-z]{2}?";
         public static string REGEX_NUMBER = "^[1-9]{1}[0-9]{1}\\s[1-9]{1}[0-9]{9}$";
         public static string REGEX_PASSWORD = "(?=.*[A-Z])(?=.*[0-9])(?=.?[@#$%^&-+=().]{1}).{8,}";
         public bool validateFirstName(string fName)
