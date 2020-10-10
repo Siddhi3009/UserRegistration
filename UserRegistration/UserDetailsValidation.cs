53769b466833ed78577937803a0be884c0699d11
Administrator <siddhiseth3009@gmail.com>
2020-10-10 14:03:59 +0530

    [Siddhi]Add TC1 - TC10 added

--- UserRegistration/UserDetailsValidation.cs
+++ UserRegistration/UserDetailsValidation.cs
@@ -5,30 +5,30 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    class UserDetailsValidation
+    public class UserDetailsValidation
     {
         public static string REGEX_FIRST_NAME = "^[A-Z][a-zA-Z]{2,}$";
         public static string REGEX_LAST_NAME = "^[A-Z][a-zA-Z]{2,}$";
         public static string REGEX_EMAIL = "^[a-zA-Z0-9]+([-.+_#$][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
         public static string REGEX_NUMBER = "^[1-9][0-9]\\s[1-9]{1}[0-9]{9}$";
         public static string REGEX_PASSWORD = "((?=.*[A-Z])(?=.*[0-9])(?=^[a-zA-Z0-9]*[@$#^!-+][a-zA-Z0-9]*$)).{8,}";
-        public bool validateFirstName(string fName)
+        public bool ValidateFirstName(string fName)
         {
             return Regex.IsMatch(fName, REGEX_FIRST_NAME);
         }
-        public bool validateLastName(string lName)
+        public bool ValidateLastName(string lName)
         {
             return Regex.IsMatch(lName, REGEX_LAST_NAME);
         }
-        public bool validateEmail(string email)
+        public bool ValidateEmail(string email)
         {
             return Regex.IsMatch(email, REGEX_EMAIL);
         }
-        public bool validateNumber(string number)
+        public bool ValidateNumber(string number)
         {
             return Regex.IsMatch(number, REGEX_NUMBER);
         }
-        public bool validatePassword(string password)
+        public bool ValidatePassword(string password)
         {
             return Regex.IsMatch(password, REGEX_PASSWORD);
         }
---
+++ UserRegistrationTest/UnitTest1.cs
@@ -0,0 +1,119 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistration;
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidateFirstName_Should_return_true_on_valid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = true;
+            //Act
+            bool result = validate.ValidateFirstName("Siddhi");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateFirstName_Should_return_false_on_invalid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = false;
+            //Act
+            bool result = validate.ValidateFirstName("siddhi");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateLastName_Should_return_true_on_valid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = true;
+            //Act
+            bool result = validate.ValidateLastName("Seth");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateLastName_Should_return_false_on_invalid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = false;
+            //Act
+            bool result = validate.ValidateLastName("seth");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateEmail_Should_return_true_on_valid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = true;
+            //Act
+            bool result = validate.ValidateEmail("Siddhi.seth3009@gmail.com.in");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateEmail_Should_return_false_on_invalid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = false;
+            //Act
+            bool result = validate.ValidateEmail("siddhi@abc@gmail.co.in.au");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateNumber_Should_return_true_on_valid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = true;
+            //Act
+            bool result = validate.ValidateNumber("91 6958348751");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidateNumber_Should_return_false_on_invalid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = false;
+            //Act
+            bool result = validate.ValidateNumber("12548963");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidatePassword_Should_return_true_on_valid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = true;
+            //Act
+            bool result = validate.ValidatePassword("Sg753eyt@fhmD");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ValidatePassword_Should_return_false_on_invalid_input()
+        {
+            //Arrange
+            UserDetailsValidation validate = new UserDetailsValidation();
+            bool expected = false;
+            //Act
+            bool result = validate.ValidatePassword("S348gbjdFTY");
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
